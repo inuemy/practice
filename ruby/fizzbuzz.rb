79ec6b2f9de13c54a7f93bd80850988c9d4dc23f
inuemy <107013381+inuemy@users.noreply.github.com>
2023-02-25 17:35:59 +0000

    fizzbuzz

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,16 +1,16 @@
 def fizz_buzz(number)
-  if number % 15 == 0
+  if number%15 == 0
     "FizzBuzz"
-  elsif number % 5 == 0
-    "Buzz"
-  elsif number % 3 == 0
+  elsif number%3 == 0
     "Fizz"
+  elsif number%5 == 0
+    "Buzz"
   else
     number.to_i
   end
 end
 
-puts "数字を入力してください。"
+puts "数字を入力してください"
 
 input = gets.to_i
 
